6e63494d30dcbf54d5fd08cbb83e180b25d37f43
Alex <alexsiri7@gmail.com>
2014-02-23 08:41:37 +0000

    Added another step

--- app/controllers/step_controller.rb
+++ app/controllers/step_controller.rb
@@ -1,16 +1,22 @@
 class StepController < ApplicationController
 
+  STEPS = ['introduction',
+            'news websites', 'reddit', 'hackernews', 'joshuakemp',
+            'sites for finding help',
+            'sites for learning']
+
   def index
-    redirect_to step_path(1)
+    redirect_to step_path(0)
   end
 
   def step
-    step_n = params.require(:step).to_i
-    @step = partial_name(step_n)
-    @next = step_n+1
-    if step_n > 100
+    @step_n = params.require(:step).to_i
+    @step = partial_name(@step_n)
+    @step_name = STEPS[@step_n]
+    @next = @step_n+1
+    if @step_n > 100
       redirect_to :finished
-    elsif !step_exists(step_n)
+    elsif !step_exists(@step_n)
       render :missing
     end
   end
@@ -20,12 +26,12 @@ class StepController < ApplicationController
   private
 
   def step_exists(step_n)
-    lookup_context.template_exists?(partial_name(step_n), 'step', true)
+    lookup_context.template_exists?(partial_name(step_n), 'step/steps', true)
   end
   helper_method :step_exists
 
   def partial_name(step_n)
-    "step_#{step_n}"
+    "#{STEPS[step_n].gsub(' ', '_')}"
   end
 
 end
