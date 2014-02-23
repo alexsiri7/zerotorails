eb6e7dbe08ea694a45b44a159c398ae3ea3499cb
Alex <alexsiri7@gmail.com>
2014-02-23 06:46:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zerotorails::Application.config.secret_key_base = '2509f95be7ca7e504e989a6181835b12b4784c0765eec7e45dc60716328dd47ff7f85653cda7f15e1453238d3a3651e03baadfd3bc93698662a23623dbdd63e6'
